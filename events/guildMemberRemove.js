0ac19eeed22da3920a772557bb62b5ad37f7b453
hiyarcocuk32 <43614317+hiyarcocuk32@users.noreply.github.com>
2018-10-29 11:44:41 +0200

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,5 +1,5 @@
 module.exports = member => {
   let guild = member.guild;
-  member.send('niye çıkıyon amk oç!');
-  guild.defaultChannel.sendMessage(`${member.user.username} isimli niye çıktın eğlenceyi kaçırdın`);
+  member.send('Ne gidiyon Tirrek');
+  guild.defaultChannel.sendMessage(`${member.user.username} gitti.`);
 };
