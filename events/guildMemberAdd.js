53e577a69a5218929da9ffd2a4d2912baf61bef0
hiyarcocuk32 <43614317+hiyarcocuk32@users.noreply.github.com>
2018-10-29 11:45:48 +0200

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
-﻿module.exports = member => {
+module.exports = member => {
     let username = member.user.username;
-    member.sendMessage('**Hoş geldin **' + username + '**!');
-    member.guild.defaultChannel.send(''+username+'İsimli HG Sunucya!!!');
+    member.sendMessage('Hoş geldin **' + username + '**!');
+    member.guild.defaultChannel.send('hg '+username+'');
 };
