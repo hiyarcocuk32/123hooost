ff762c173e53b6d487497295506ec1ab6322435d
hiyarcocuk32 <43614317+hiyarcocuk32@users.noreply.github.com>
2018-10-29 11:00:12 +0200

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -7,14 +7,14 @@ exports.run = (client, message, params) => {
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.author.sendCode('asciidoc', `= Komut Listesi =\n\n[Komut hakkinda bilgi için ${ayarlar.prefix}yardIm <komut adi>]\n\n${client.commands.map(c => `${ayarlar.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`);
+    message.author.send('asciidoc', `= Komut Listesi =\n\n[Komut hakkinda bilgi için ${ayarlar.prefix}yardIm <komut adi>]\n\n${client.commands.map(c => `${ayarlar.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`);
   if (message.channel.type !== 'dm') {
     const ozelmesajkontrol = new Discord.RichEmbed()
     .setColor(0x00AE86)
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
     .setDescription('Özel mesajlarını kontrol et. :postbox:');
-    message.channel.sendEmbed(ozelmesajkontrol) }
+    message.channel.send(ozelmesajkontrol) }
   } else {
     let command = params[0];
     if (client.commands.has(command)) {
